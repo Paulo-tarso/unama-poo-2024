e27d4c1c77b247774dcf8b3526d53bb4c59c4a48
Paulo-tarso <paulotarso6096@gmail.com>
2024-08-22 00:52:50 +0000

    add: BalancoTrimestral

---
+++ Testeidade.java
@@ -0,0 +1,32 @@
+class Testeidade{
+    public static void main(String[] args){
+        int idade;
+        idade = 29;
+        System.out.println(idade); // Imprime idade
+        
+        int idadeAnoQueVem = idade + 1;
+
+        System.out.println(idadeAnoQueVem);
+
+        double pi = 3.14;
+        int raio = 5;
+        double resultado = 2 * raio * pi;
+
+        System.out.println(resultado == 31.4);
+
+        boolean menorDeidade = idade >= 18;
+
+        System.out.println(menorDeidade);
+
+        char letra = 'a';
+
+        System.out.println(letra);
+
+        int i = 5; // i : 5
+        int j = 1; // j : 5
+        i = i + 1; 
+
+        System.out.println(i,j);
+
+    }
+}
